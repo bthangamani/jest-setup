c5b80addc550b372873edea42cddd11d3eb541f4
bthangamani <boopathi.thangamani.contractor@realtor.com>
2024-03-15 13:45:08 +0530

    axios test

---
+++ __test__/src/users.test.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import Users from '../../src/users.js';
+import { jest } from '@jest/globals';
+// jest.mock('axios', jest.fn(() => {
+//   return {
+//     get: jest.fn()
+//   }
+// }));
+
+axios = jest.fn(() => {
+  return {
+    get: jest.fn()
+  }
+});
+
+test('should fetch users', () => {
+  const users = [{ name: 'Bob' }];
+  const resp = { data: users };
+  axios.get.mockResolvedValue(resp);
+
+  // or you could use the following depending on your use case:
+  // axios.get.mockImplementation(() => Promise.resolve(resp))
+
+  return Users.all().then(data => expect(data).toEqual(users));
+});
